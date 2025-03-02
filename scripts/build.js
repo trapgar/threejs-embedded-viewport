c142dcedd92c2070d50c9edbbad6e28658ca1a33
Tyler <trapgar@gmail.com>
2025-03-02 16:01:36 -0600

    added simple debug stuff

---
+++ scripts/build.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { execSync } = require('child_process');
+const debounce = require('./utils/debounce');
+
+const PATH_SRC = path.join(__dirname, '..', 'src');
+const watch = process.argv.find(a => a.startsWith('--watch'));
+
+const cmd = [
+    '.\\node_modules\\.bin\\esbuild',
+    'src/index.ts',
+    '--bundle',
+    '--minify',
+    '--outfile=dist/three.editor.min.js',
+];
+
+if (watch)
+    cmd.splice(cmd.length - 1, 0, '--sourcemap');
+
+const build = () => {
+    console.clear();
+    execSync(cmd.join(' '), { stdio: 'inherit' });
+};
+
+if (watch) {
+    fs.watch(PATH_SRC, {}, debounce((eventType) => {
+        if (eventType === 'change')
+            build();
+    }, 500));
+}
+
+build();
--- src/Editor.ts
+++ src/Editor.ts
@@ -1,142 +1,113 @@
-import * as THREE from 'three';
-import Signal from './Signal';
+import { BoxGeometry, ColorRepresentation, EventDispatcher, GridHelper, Group, Mesh, MeshBasicMaterial, Object3D, Object3DEventMap, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
+import EditorControls from './EditorControls';
 
-export type EditorSignals = {
-    // script
-    editScript: Signal;
-    // player
-    startPlayer: Signal; stopPlayer: Signal;
-    // xr
-    enterXR: Signal; offerXR: Signal; leaveXR: Signal;
-    // notifications
-    editorCleared: Signal; savingStarted: Signal; savingFinished: Signal; transformModeChanged: Signal; snapChanged: Signal; spaceChanged: Signal; rendererCreated: Signal; rendererUpdated: Signal; rendererDetectKTX2Support: Signal; sceneBackgroundChanged: Signal; sceneEnvironmentChanged: Signal; sceneFogChanged: Signal; sceneFogSettingsChanged: Signal; sceneGraphChanged: Signal; sceneRendered: Signal; cameraChanged: Signal; cameraResetted: Signal; geometryChanged: Signal; objectSelected: Signal; objectFocused: Signal; objectAdded: Signal; objectChanged: Signal; objectRemoved: Signal; cameraAdded: Signal; cameraRemoved: Signal; helperAdded: Signal; helperRemoved: Signal; materialAdded: Signal; materialChanged: Signal; materialRemoved: Signal; scriptAdded: Signal; scriptChanged: Signal; scriptRemoved: Signal; windowResize: Signal; showGridChanged: Signal; showHelpersChanged: Signal; refreshSidebarObject3D: Signal; refreshSidebarEnvironment: Signal; historyChanged: Signal; viewportCameraChanged: Signal; viewportShadingChanged: Signal; intersectionsDetected: Signal;
+type ColorScheme = 'dark' | 'light';
+
+type EditorColorThemes = {
+    [K in ColorScheme]: {
+        background: [hex: ColorRepresentation, alpha: number];
+        grid: [hex: number, alpha: number];
+    };
 };
 
-export default class Editor {
-    renderer: THREE.WebGLRenderer;
-    scene: THREE.Scene;
-    animations: FrameRequestCallback[] = [];
-    camera: THREE.PerspectiveCamera;
-    signals: EditorSignals;
+const THEMES: EditorColorThemes = {
+    'dark': { background: [0x000000, 0], grid: [0x555555, 0x888888] },
+    'light': { background: [0xaaaaaa, 1], grid: [0x999999, 0x777777] },
+};
 
-    get width() { return window.innerWidth - 20 * 2; };
-    get height() { return window.innerHeight - 20; };
+const CAMERA_DEFAULT = new PerspectiveCamera(50, 1, 0.01, 1000);
+CAMERA_DEFAULT.name = 'Camera';
+CAMERA_DEFAULT.position.set(0, 5, 10);
+CAMERA_DEFAULT.lookAt(new Vector3());
+
+export default class Editor extends EventDispatcher {
+    renderer: WebGLRenderer;
+    scene: Scene;
+    animations: FrameRequestCallback[] = [];
+    camera: PerspectiveCamera;
+    grid: Group<Object3DEventMap>;
+    pid: number = -1;
+    controls: EditorControls;
+
+    overlays: Object3D[] = [];
+
+    get theme(): ColorScheme { return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'; };
 
     constructor(root: HTMLElement) {
-        console.log('Hello, TypeScript!');
+        super();
         this.tick = this.tick.bind(this);
+        this.render = this.render.bind(this);
 
-        this.signals = {
+        console.log(`js Editor will use color scheme %c${this.theme}`, 'font-weight: bold');
+        console.log('Baz');
 
-            // script
+        const { width, height } = root.getBoundingClientRect();
+        const renderer = this.renderer = new WebGLRenderer({ antialias: true })
+        renderer.setSize(width, height);
+        const colours = THEMES[this.theme];
+        renderer.setClearColor(colours.background[0], colours.background[1]);
 
-            editScript: new Signal(),
+        const scene = this.scene = new Scene();
+        const camera = this.camera = CAMERA_DEFAULT.clone();
 
-            // player
+        this.controls = new EditorControls(camera, renderer.domElement);
+        this.controls.addEventListener('change', this.render);
 
-            startPlayer: new Signal(),
-            stopPlayer: new Signal(),
-
-            // xr
-
-            enterXR: new Signal(),
-            offerXR: new Signal(),
-            leaveXR: new Signal(),
-
-            // notifications
-
-            editorCleared: new Signal(),
-
-            savingStarted: new Signal(),
-            savingFinished: new Signal(),
-
-            transformModeChanged: new Signal(),
-            snapChanged: new Signal(),
-            spaceChanged: new Signal(),
-            rendererCreated: new Signal(),
-            rendererUpdated: new Signal(),
-            rendererDetectKTX2Support: new Signal(),
-
-            sceneBackgroundChanged: new Signal(),
-            sceneEnvironmentChanged: new Signal(),
-            sceneFogChanged: new Signal(),
-            sceneFogSettingsChanged: new Signal(),
-            sceneGraphChanged: new Signal(),
-            sceneRendered: new Signal(),
-
-            cameraChanged: new Signal(),
-            cameraResetted: new Signal(),
-
-            geometryChanged: new Signal(),
-
-            objectSelected: new Signal(),
-            objectFocused: new Signal(),
-
-            objectAdded: new Signal(),
-            objectChanged: new Signal(),
-            objectRemoved: new Signal(),
-
-            cameraAdded: new Signal(),
-            cameraRemoved: new Signal(),
-
-            helperAdded: new Signal(),
-            helperRemoved: new Signal(),
-
-            materialAdded: new Signal(),
-            materialChanged: new Signal(),
-            materialRemoved: new Signal(),
-
-            scriptAdded: new Signal(),
-            scriptChanged: new Signal(),
-            scriptRemoved: new Signal(),
-
-            windowResize: new Signal(),
-
-            showGridChanged: new Signal(),
-            showHelpersChanged: new Signal(),
-            refreshSidebarObject3D: new Signal(),
-            refreshSidebarEnvironment: new Signal(),
-            historyChanged: new Signal(),
-
-            viewportCameraChanged: new Signal(),
-            viewportShadingChanged: new Signal(),
-
-            intersectionsDetected: new Signal(),
-
-        };
-
-        const renderer = this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize(this.width, this.height);
-        renderer.setClearColor(0xdddddd, 1);
-
-        const scene = this.scene = new THREE.Scene();
-        const camera = this.camera = new THREE.PerspectiveCamera(70, this.width / this.height);
-        camera.position.z = 50;
         scene.add(camera);
+
+        const grid = this.grid = new Group();
+        // 1 tick every unit
+        const grid1 = new GridHelper(100, 100);
+        grid1.material.color.setHex(colours.grid[0]);
+        grid1.material.vertexColors = false;
+        grid.add(grid1);
+
+        // 1 tick every 5 units
+        const grid2 = new GridHelper(100, 20);
+        grid2.material.color.setHex(colours.grid[1]);
+        grid2.material.vertexColors = false;
+        grid.add(grid2);
+
+        this.overlays.push(grid);
 
         root.innerHTML = '';
         root.appendChild(this.renderer.domElement);
 
-        this.tick(0);
+        // this.tick(0);
     }
 
     scaffold() {
-        const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
-        const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x0095dd });
-        const cube = new THREE.Mesh(boxGeometry, basicMaterial);
+        const boxGeometry = new BoxGeometry();
+        const basicMaterial = new MeshBasicMaterial({ color: 0x0095dd });
+        const cube = new Mesh(boxGeometry, basicMaterial);
+        cube.position.set(0, 0.5, 0);
         this.scene.add(cube);
-        cube.rotation.set(0.4, 0.2, 0);
 
-        const light = new THREE.PointLight(0xffffff);
-        light.position.set(-10, 15, 50);
-        this.scene.add(light);
+        // const light = new PointLight(0xffffff);
+        // light.position.set(-10, 15, 50);
+        // this.scene.add(light);
 
         // 180 / 10 deg per second
         this.animations.push(delta => cube.rotation.y = delta / (180 * 10));
+        this.render();
+    }
+
+    startTime = 0;
+    endTime = 0;
+
+    render() {
+        this.startTime = performance.now();
+        this.renderer.render(this.scene, this.camera);
+        this.endTime = performance.now();
+
+        this.renderer.autoClear = false;
+        for (const overlay of this.overlays)
+            this.renderer.render(overlay, this.camera);
+        this.renderer.autoClear = true;
     }
 
     tick(delta: DOMHighResTimeStamp) {
-        requestAnimationFrame(this.tick);
+        this.pid = requestAnimationFrame(this.tick);
 
         this.animations.forEach(cb => cb(delta));
 
